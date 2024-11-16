8cf25d23ee9ace1b145087673dc29f6a705322aa
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-11-16 14:23:54 -0300

    Initial Commit

---
+++ Quartet-game/Quartet_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quartet_gameApp.swift
+//  Quartet-game
+//
+//  Created by Bruno Oliveira on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Quartet_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
